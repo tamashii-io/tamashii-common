5f3f87c5a33396a23efd3a623e6bd03423e5c267
pi <lctseng@5xruby.tw>
2016-12-21 06:34:35 +0000

    add constants, fix env initialize error

--- lib/codeme/resolver.rb
+++ lib/codeme/resolver.rb
@@ -1,10 +1,9 @@
 require 'codeme/packet'
 
-
 module  Codeme
   class Handler
     def initialize(env = {})
-      @env = {}           
+      @env = env          
     end
 
     def resolve(action_code, data = nil)
@@ -17,11 +16,23 @@ module Codeme
 
   TYPE_CODE_SYSTEM = 0 << 3
   TYPE_CODE_AUTH = 1 << 3
-  TYPE_CODE_RFID = 2 << 3
-  TYPE_CODE_BUZZER = 3 << 3
+  TYPE_CODE_RFID = 3 << 3
+  TYPE_CODE_BUZZER = 4 << 3
+
+
+  ACTION_CODE_SYSTEM_POWEROFF = 0
+  ACTION_CODE_SYSTEM_REBOOT = 0
 
   ACTION_CODE_AUTH_TOKEN = 0
   ACTION_CODE_AUTH_RESULT = 7
+
+  ACTION_CODE_RFID_NUMBER = 0
+  ACTION_CODE_RFID_DATA = 1
+  ACTION_CODE_RFID_RESULT_JSON = 6
+  ACTION_CODE_RFID_RESULT_STRING = 7
+
+  ACTION_CODE_BUZZER_SOUND = 0
+
   module Resolver
     module_function
 
