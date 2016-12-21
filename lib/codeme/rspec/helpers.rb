528089ed19d9874f1ff9af6a85683427460e362f
蒼時弦也 <elct9620@frost.tw>
2016-12-21 16:29:29 +0800

    Fix websocket mask method not split to array

--- lib/codeme/rspec/helpers.rb
+++ lib/codeme/rspec/helpers.rb
@@ -21,12 +21,12 @@ module Codeme
 
       def codeme_binary_packet(type, tag, body)
         packet = codeme_packet(type, tag, body)
-        [0x82, 0x80 + packet.size] + websocket_mask + websocket_mask_message(packet)
+        [0x82, 0x80 + packet.size] + websocket_mask + websocket_mask_message(*packet)
       end
 
       def codeme_text_packet(type, tag, body)
         packet = codeme_packet(type, tag, body)
-        [0x81, 0x80 + packet.size] + websocket_mask + websocket_mask_message(packet)
+        [0x81, 0x80 + packet.size] + websocket_mask + websocket_mask_message(*packet)
       end
     end
   end
