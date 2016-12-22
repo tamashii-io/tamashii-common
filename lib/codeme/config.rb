da6da1792b6c1900fa17a21729386bd4d71f0240
蒼時弦也 <elct9620@frost.tw>
2016-12-22 16:21:28 +0800

    Adjust to user attr_accessor to split default config between child class

--- lib/codeme/config.rb
+++ lib/codeme/config.rb
@@ -3,30 +3,21 @@ module Codeme
     SHARED_CONFIG = %i(auth_type token log_file log_level token env).freeze
 
     class << self
+      attr_accessor :default_config
+
       def method_missing(name, *args, &block)
         (@instance ||= Config.new).send(name, *args, &block)
       end
 
-      def inherited(base)
-        base.class_variable_set(:@@accept_config, {})
-      end
-
       def register(name, default = nil)
-        accept_config = self.class_variables_get(:@@accept_config) || {}
-        accept_config[name.to_sym] = default
-        self.class_variable_set(:@@accept_config, accept_config)
-      end
-
-      def load_default
-        self.class_variable_get(:@@accept_config)
-      rescue NameError
-        {}
+        self.default_config ||= {}
+        self.default_config[name.to_sym] = default
       end
     end
 
     def initialize
       @accept_config = Set.new(SHARED_CONFIG)
-      self.class.load_default.each { |name, value| register(name, value) }
+      (self.class.default_config || {}).each { |name, value| register(name, value) }
     end
 
     def method_missing(name, *args, &block)
