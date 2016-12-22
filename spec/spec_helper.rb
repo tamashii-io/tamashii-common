9f19e02715920f6da60847ce16d35a7773804ae5
蒼時弦也 <elct9620@frost.tw>
2016-12-22 11:29:51 +0800

    Setup simplecov and guard for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "codeme/common"
+
+require 'simplecov'
+
+SimpleCov.start do
+  filters.clear
+  add_filter do |src|
+    !(src.filename =~ /^#{SimpleCov.root}/) unless src.filename =~ /codeme/
+  end
+end
