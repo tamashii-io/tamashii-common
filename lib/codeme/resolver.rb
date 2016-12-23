2bf1f44da06dc72ed7286f41904c7e9e6e5cb9a6
蒼時弦也 <elct9620@frost.tw>
2016-12-23 15:37:30 +0800

    Add default handler

--- lib/codeme/resolver.rb
+++ lib/codeme/resolver.rb
@@ -8,6 +8,11 @@ module Codeme
       class_eval(&block)
     end
 
+    def default_handler(handler_class = nil, env = nil)
+      return @default_handler || [nil, nil] if handler_class.nil?
+      @default_handler = [handler_class, env]
+    end
+
     def handle(type_code, handler_class, options = {})
       raise NotImplementedError.new("Handler should implement resolve method") unless handler_class.method_defined?(:resolve)
       @handlers ||= {}
@@ -16,7 +21,7 @@ module Codeme
 
     def resolve(pkt, env = {})
       @handlers ||= {}
-      handler, options = @handlers[pkt.type] || [nil, nil]
+      handler, options = @handlers[pkt.type] || @default_handler
       handler.new(pkt.type, options.merge(env)).resolve(pkt.body) if handler
     end
 
