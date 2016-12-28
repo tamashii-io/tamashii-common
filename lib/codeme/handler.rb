e6867e8d22dd80b1a84c52df60517d732406d7b5
Liang-Chi Tseng <lctseng@5xruby.tw>
2016-12-28 04:23:33 +0000

    add handler spec, fix attribute name: type -> type_code

--- lib/codeme/handler.rb
+++ lib/codeme/handler.rb
@@ -1,13 +1,13 @@
 module  Codeme
   class Handler
     attr_reader :type_code, :env
-    def initialize(type, env = {})
-      @type = type
+    def initialize(type_code, env = {})
+      @type_code = type_code
       @env = env
     end
 
     def resolve(action_code, data = nil)
-      raise NotImplementError.new("The resolve method should be implemented.")
+      raise NotImplementedError.new("The resolve method should be implemented.")
     end
   end
 end
