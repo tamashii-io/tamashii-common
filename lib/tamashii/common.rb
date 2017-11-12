802066e85380a586b4f707b1b29a1dc59c34b91e
蒼時弦也 <elct9620@frost.tw>
2017-11-12 23:29:50 +0800

    Remove config to prevent conflict with tamashii-config

--- lib/tamashii/common.rb
+++ lib/tamashii/common.rb
@@ -5,7 +5,6 @@ require "tamashii/agent_hint"
 require "tamashii/handler"
 require "tamashii/resolver"
 require "tamashii/logger"
-require "tamashii/config"
 require "tamashii/environment"
 
 module Tamashii
